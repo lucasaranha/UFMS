e1ec94e244bd30db43134329d393063438dd82c2
Lucas Aranha <lucas_aranha8@hotmail.com>
2020-03-28 14:52:46 -0400

    Update intervalo100.c

--- ALG I - Lista 3/intervalo100.c
+++ ALG I - Lista 3/intervalo100.c
@@ -5,7 +5,7 @@ int main(void){
 	for(int i = 100; i <= 1000; i++){
 		int j = i;
 		while(j > 0){
-			aux = j%10;
+			aux = j%10; // pegando o numero mais à direita, exemplo: 1395. pega primeiro o 5, depois o j
 			soma_digitos += aux; //somando os digitos do numero
 			j = j/10;
 		}
