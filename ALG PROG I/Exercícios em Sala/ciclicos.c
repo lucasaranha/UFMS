e7a898bbc4dca578834b4e5b5d2b51274cbef5f8
Lucas Aranha <lucas_aranha8@hotmail.com>
2020-04-08 01:38:43 -0400

    Update

--- ALG PROG I/Exercícios em Sala/ciclicos.c
+++ ALG PROG I/Exercícios em Sala/ciclicos.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 int main(void){
-	int num1, num2, soma1, soma2, aux1, aux2;
-	soma1 = soma2 = 0;
-		printf("Digite dois número: ");
-		scanf("%d%d", &num1, &num2);
+	int num1, num2, copia, aux, digitos, pot = 1;
+	printf("Digite dois números: ");
+	scanf("%d %d", &num1, &num2);
+	copia = num2;
 
-		while(num1 > 0 && num2 > 0){
-			aux1 = num1%10;
-			soma1 += aux1;
-			num1 /= 10;
-
-			aux2 = num2%10;
-			soma2 += aux2;
-			num2 /= 10;
+		while(copia > 0){
+			aux = copia%10;
+			digitos++;
+			copia /= 10;
+			pot *= 10;
 		}
+	copia = num2;
+	pot /= 10;
+
+		do
+		{
+			aux = copia/pot;
+			copia = ((copia - (aux * pot)) * 10) + aux;
+			if(num1 == copia) break;
+		} while (digitos--);
+
+
+		if(num1 == copia) printf("É ciclíco\n");
+		else printf("Não é cíclico\n");
 		
-		if(soma1 == soma2) printf("Os números são cíclicos\n");
-		else printf("Não são cíclicos\n");
+
 
 return 0;
 }
